8a3f023a6d1733c2da9cf902370faf314ec03355
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-11-24 10:16:13 -0300

    feat(prisma): "module" & "service" created

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsFindAllModule } from './products/products-findAll/product-findAll.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ProductsFindAllModule],
+  imports: [ProductsFindAllModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
