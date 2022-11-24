efcbaecc665496efb2cd12908df3d9d0faf39bfa
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-11-24 15:32:00 -0300

    fix: updated method "product update"

--- src/products/product-update/product-update.module.ts
+++ src/products/product-update/product-update.module.ts
@@ -1,11 +1,13 @@
 import { Module } from "@nestjs/common";
 import { PrismaModule } from "src/prisma/prisma.module";
 import { ProductFindOneModule } from "../product-findOne/product-findOne.module";
+import { ProductUpdateController } from "./product-update.controller";
+import { ProductUpdateService } from "./product-update.service";
 
 
 @Module({
     imports: [ProductFindOneModule, PrismaModule],
-    controllers: [],
-    providers: [],
+    controllers: [ProductUpdateController],
+    providers: [ProductUpdateService],
 })
 export class ProductUpdateModule {}
